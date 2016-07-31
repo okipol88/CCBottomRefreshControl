1d98e78d9384835b33fa208fa6d2330bd34bced2
Błażej Szajrych <blazej.szajrych@gmail.com>
2016-07-31 22:20:27 +0200

    Added carthage based masonry support.

---
+++ Framework/CCbottomControl/CCbottomControl/CCbottomControl.h
@@ -0,0 +1,20 @@
+//
+//  CCbottomControl.h
+//  CCbottomControl
+//
+//  Created by Błażej Szajrych on 31.07.2016.
+//  Copyright © 2016 vlasov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CCbottomControl.
+FOUNDATION_EXPORT double CCbottomControlVersionNumber;
+
+//! Project version string for CCbottomControl.
+FOUNDATION_EXPORT const unsigned char CCbottomControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+#import <CCbottomControl/UIScrollView+BottomRefreshControl.h>
+
+
